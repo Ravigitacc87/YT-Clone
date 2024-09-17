3a919b9f5ef17e693598a62f5979bc4caada94d9
Ravi <166257838+Ravigitacc87@users.noreply.github.com>
2024-09-17 21:34:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ var sidebar = document.querySelector(".sidebar");
 var mainContainer = document.querySelector(".maincontainer");
 var cards = document.querySelectorAll(".card"); // Select all cards
 var info =document.querySelectorAll(".video-information");
-
-menuIcon.onclick = function() {
-    // Toggle the sidebar and main container classes
-    sidebar.classList.toggle("smaller-sidebar");
-    mainContainer.classList.toggle("larger-maincontainer");
-
-    // Toggle the card classes for resizing
-    cards.forEach(card => {
-        card.classList.toggle("smaller-card");
+                
+  menuIcon.onclick = function() {
+ // Toggle the sidebar and main container classes
+ sidebar.classList.toggle("smaller-sidebar");
+ mainContainer.classList.toggle("larger-maincontainer");
+        
+ // Toggle the card classes for resizing
+ cards.forEach(card => {
+   card.classList.toggle("smaller-card");
     });
-    info.forEach(infosmall => {
-        infosmall.classList.toggle("video-information-smaller");
+ info.forEach(infosmall => {
+    infosmall.classList.toggle("video-information-smaller");
     });
-};
+ };
